72d8600349a969668a7a71713a46a0297ce9acce
Steven Penny <srpen6+1268@gmail.com>
2023-09-11 17:45:57 -0500

    +type Module struct {

--- widevine_test.go
+++ widevine_test.go
@@ -1,40 +1,33 @@
 package widevine
 
 import (
-   "bytes"
    "encoding/base64"
-   "encoding/json"
    "fmt"
    "net/http"
    "os"
    "testing"
 )
 
-func (test_post) _Request_URL() string {
-   return "https://integration.widevine.com/_/license_response"
+// therokuchannel.roku.com/watch/597a64a4a25c5bf6af4a8c7053049a6f
+const post_pssh = "AAAAQ3Bzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAACMIARIQKDOa149zRSDaJObgVz05LhoKaW50ZXJ0cnVzdCIBKg=="
+
+func (roku) _Request_URL() string {
+   return "https://wv-license.sr.roku.com/license/v1/license/wv?token=Lc1ODa8rmoLJIqcK_qX5Qx6AVI1Ukeh20aUY13fyTZkMNDilufUl_UvdF_tcakp6teHDb5yBYZVjsXIt-Gadl_bOJVzEFIhNZCMSYh7aO0KM9HrY2G-mfm3sHQLEUulP5Cd3a2TNFZdJV2Xv5_TnOIJpyU1jTuDs16uvOkRvsJ6luRagJR0y-J-EJmocwUH4WrRZ8lFrzMQ2u3-AGrN_vFtGgx390fhQp7tLH4ImInykc6MtASyTpO0XOD1BvIC6_aF5ghOux3OOTTj_XXadIDT74Fo6NbFZ8gXzwcSSNbT_830Kz4Sdqmpevk2lytcuF2E46KYh56YvjpAu5YkZp04fVAOv_xYajw==&traceId=8dd3b05b934e8b215d86d3d8fce8e430&ExpressPlayToken=none"
 }
 
-func (t test_post) _Response_Body(s []byte) ([]byte, error) {
-   _, s, _ = bytes.Cut(s, []byte{'\n'})
-   var v struct {
-      Client_ID struct {
-         Token struct {
-            Public_Key []byte `json:"publicKey"`
-         }
-      } `json:"clientId"`
-   }
-   err := json.Unmarshal(s, &v)
-   if err != nil {
-      return nil, err
-   }
-   return v.Client_ID.Token.Public_Key, nil
+func (roku) _Request_Header() http.Header {
+   return nil
 }
 
-func (t test_post) _Request_Body(src []byte) ([]byte, error) {
-   buf := make([]byte, t.e.EncodedLen(len(src)))
-   t.e.Encode(buf, src)
-   return buf, nil
+func (roku) _Request_Body(b []byte) ([]byte, error) {
+   return b, nil
 }
+
+func (roku) _Response_Body(b []byte) ([]byte, error) {
+   return b, nil
+}
+
+type roku struct{}
 
 func Test_Post(t *testing.T) {
    home, err := os.UserHomeDir()
@@ -49,8 +42,7 @@ func Test_Post(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   test := test_post{base64.StdEncoding}
-   pssh, err := test.e.DecodeString(post_pssh)
+   pssh, err := base64.StdEncoding.DecodeString(post_pssh)
    if err != nil {
       t.Fatal(err)
    }
@@ -58,17 +50,10 @@ func Test_Post(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   key, err := mod._Post(test)
+   key, err := mod._Post(roku{})
    if err != nil {
       t.Fatal(err)
    }
    fmt.Printf("%x\n", key)
 }
 
-func (test_post) _Request_Header() http.Header { return nil }
-
-const post_pssh = "AAAARHBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAACQIARIBNRoNd2lkZXZpbmVfdGVzdCIKMjAxNV90ZWFycyoCU0Q="
-
-type test_post struct {
-   e *base64.Encoding
-}
