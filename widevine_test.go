133a3547bd388bd0346318ccbfbd994a4503678a
Steven Penny <srpen6+1268@gmail.com>
2023-09-11 19:29:07 -0500

    +   Request_Header() http.Header

--- widevine_test.go
+++ widevine_test.go
@@ -15,15 +15,15 @@ func (roku) Request_URL() string {
 	return "https://wv-license.sr.roku.com/license/v1/license/wv?token=Lc1ODa8rmoLJIqcK_qX5Qx6AVI1Ukeh20aUY13fyTZkMNDilufUl_UvdF_tcakp6teHDb5yBYZVjsXIt-Gadl_bOJVzEFIhNZCMSYh7aO0KM9HrY2G-mfm3sHQLEUulP5Cd3a2TNFZdJV2Xv5_TnOIJpyU1jTuDs16uvOkRvsJ6luRagJR0y-J-EJmocwUH4WrRZ8lFrzMQ2u3-AGrN_vFtGgx390fhQp7tLH4ImInykc6MtASyTpO0XOD1BvIC6_aF5ghOux3OOTTj_XXadIDT74Fo6NbFZ8gXzwcSSNbT_830Kz4Sdqmpevk2lytcuF2E46KYh56YvjpAu5YkZp04fVAOv_xYajw==&traceId=8dd3b05b934e8b215d86d3d8fce8e430&ExpressPlayToken=none"
 }
 
-func (roku) _Request_Header() http.Header {
+func (roku) Request_Header() http.Header {
 	return nil
 }
 
-func (roku) Encode_Request(b []byte) ([]byte, error) {
+func (roku) Request_Body(b []byte) ([]byte, error) {
 	return b, nil
 }
 
-func (roku) Decode_Response(b []byte) ([]byte, error) {
+func (roku) Response_Body(b []byte) ([]byte, error) {
 	return b, nil
 }
 
