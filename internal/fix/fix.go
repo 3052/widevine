b321c3613bf8d5e28f53d82f2487366eb73885c1
3052 <spen+3052@protonmail.com>
2024-02-25 22:55:19 -0600

    +   "bufio"

--- internal/fix/fix.go
+++ internal/fix/fix.go
@@ -2,28 +2,41 @@ package main
 
 import (
    "154.pages.dev/protobuf"
-   "bytes"
+   "bufio"
+   "fmt"
+   "io"
+   "net/http"
    "os"
 )
 
 func main() {
-   data, err := os.ReadFile("req.txt")
+   req, err := func() (*http.Request, error) {
+      f, err := os.Open("req.txt")
+      if err != nil {
+         return nil, err
+      }
+      defer f.Close()
+      return http.ReadRequest(bufio.NewReader(f))
+   }()
    if err != nil {
       panic(err)
    }
-   _, data, _ = bytes.Cut(data, []byte("\r\n\r\n"))
+   body, err := io.ReadAll(req.Body)
+   if err != nil {
+      panic(err)
+   }
    var message protobuf.Message
-   if err := message.Consume(data); err != nil {
+   if err := message.Consume(body); err != nil {
       panic(err)
    }
    message, _ = message.Get(2)
-   file, err := os.Create("peacock.go")
+   f, err := os.Create("peacock.go")
    if err != nil {
       panic(err)
    }
-   defer file.Close()
-   file.WriteString("package main\n")
-   file.WriteString("import `154.pages.dev/protobuf`\n")
-   file.WriteString("var message = ")
-   file.WriteString(message.GoString())
+   defer f.Close()
+   fmt.Fprintln(f, "package main")
+   fmt.Fprintln(f, `import "154.pages.dev/protobuf"`)
+   fmt.Fprintf(f, "var address = %q\n", req.URL.RawQuery)
+   fmt.Fprintln(f, "var message =", message.GoString())
 }
--- internal/req.go
+++ internal/req.go
@@ -17,7 +17,6 @@ import (
 )
 
 func main() {
-   return
    var req http.Request
    req.Header = make(http.Header)
    req.Header["Accept"] = []string{"*/*"}
@@ -30,14 +29,12 @@ func main() {
    req.URL = new(url.URL)
    req.URL.Host = "ovp.peacocktv.com"
    req.URL.Path = "/drm/widevine/acquirelicense"
-   val := make(url.Values)
-   val["bt"] = []string{"99-kmZraEntWbWr6LmEF_KJP3x8sZk2JDFFYwjjYRlrBaK8lKzgFJ0vId2jmQ0QMFCzVcAxESCKdL5elK63DBe5dNJwCoYYk3T1wh0_ClN6M-EGKmy6uPgjwWRhfrZWmhlmMKyiq9JQxV54-UGkG6Yne0S4U9TYtbN6tX3_sobqET3BO2J2Ux0Z1x1Z985UaaNJEydmji_GZr6Eb3LizYYCnHOg03_MAvGtE0WlCyOtIy8VQrmuT7j_dvr7-JdOWXQwDzi-Th-84Z4P-yUcOtti4bFWuxrMSfpimlnUqLRyPTQsYqHAJUpUN_9kaqwvlxFfLGxzYlY_b75k-RgUq86VwKzbbGMQTF8qJ46faR_gJModrStHS__wfvYwh0FjjygzouTmK1mt1-K6ZXhn"}
-   req.URL.RawQuery = val.Encode()
    req.URL.Scheme = "https"
    data, err := request_signed(message.Encode())
    if err != nil {
       panic(err)
    }
+   req.URL.RawQuery = address
    req.Body = io.NopCloser(bytes.NewReader(data))
    res, err := new(http.Transport).RoundTrip(&req)
    if err != nil {
