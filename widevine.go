ac0610bfe3057c49bc79fa8388db4987a2c1aa96
3052 <spen+3052@protonmail.com>
2024-03-23 22:22:16 -0500

    -// ISO/IEC 14496-12

--- widevine.go
+++ widevine.go
@@ -10,6 +10,22 @@ import (
    "net/http"
 )
 
+type PSSH struct {
+   SpecificHeader struct {
+      Size     uint32
+      Type     Type
+      Version  uint8
+      Flags    [3]byte
+      SystemId SystemId
+      DataSize uint32
+   }
+   // all of the Widevine PSSH I have seen so far are single `key_id`, so we
+   // are going to implement that for now, because its not clear what the logic
+   // would be with multiple key_ids.
+   Key_ID     []byte
+   content_id []byte
+}
+
 func (p PSSH) CDM(private_key, client_id []byte) (*CDM, error) {
    var module CDM
    // key_id
@@ -92,72 +108,3 @@ type Poster interface {
    ResponseBody([]byte) ([]byte, error)
 }
 
-// ISO/IEC 14496-12
-//
-//   aligned(8) class Box (
-//      unsigned int(32) boxtype, optional unsigned int(8)[16] extended_type
-//   ) {
-//      BoxHeader(boxtype, extended_type);
-//      // the remaining bytes are the BoxPayload
-//   }
-//
-//   aligned(8) class BoxHeader (
-//      unsigned int(32) boxtype, optional unsigned int(8)[16] extended_type
-//   ) {
-//      unsigned int(32) size;
-//      unsigned int(32) type = boxtype;
-//      if (size==1) {
-//         unsigned int(64) largesize;
-//      } else if (size==0) {
-//         // box extends to end of file
-//      }
-//      if (boxtype=='uuid') {
-//         unsigned int(8)[16] usertype = extended_type;
-//      }
-//   }
-//
-//   aligned(8) class FullBox(
-//      unsigned int(32) boxtype,
-//      unsigned int(8) v,
-//      bit(24) f,
-//      optional unsigned int(8)[16] extended_type
-//   ) extends Box(boxtype, extended_type) {
-//      FullBoxHeader(v, f);
-//      // the remaining bytes are the FullBoxPayload
-//   }
-//
-//   aligned(8) class FullBoxHeader(unsigned int(8) v, bit(24) f) {
-//      unsigned int(8) version = v;
-//      bit(24) flags = f;
-//   }
-//
-// ISO/IEC 23001-7
-//
-//   aligned(8) class ProtectionSystemSpecificHeaderBox extends FullBox(
-//      'pssh', version, flags=0,
-//   ) {
-//      unsigned int(8)[16] SystemID;
-//      if (version > 0) {
-//         unsigned int(32) KID_count;
-//         {
-//            unsigned int(8)[16] KID;
-//         } [KID_count];
-//      }
-//      unsigned int(32) DataSize;
-//      unsigned int(8)[DataSize] Data;
-//   }
-type PSSH struct {
-   SpecificHeader struct {
-      Size     uint32
-      Type     Type
-      Version  uint8
-      Flags    [3]byte
-      SystemId SystemId
-      DataSize uint32
-   }
-   // all of the Widevine PSSH I have seen so far are single `key_id`, so we
-   // are going to implement that for now, because its not clear what the logic
-   // would be with multiple key_ids.
-   Key_ID     []byte
-   content_id []byte
-}
