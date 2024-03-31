58a6038789200a895ebc198b42f08141880fd1f0
3052 <spen+3052@protonmail.com>
2024-03-31 08:41:16 -0500

    new file:   testdata/amc.go

---
+++ testdata/hulu.go
@@ -0,0 +1,9 @@
+protobuf.Message{
+protobuf.Field{Number:1, Type:0, Value:protobuf.Varint(1)},
+protobuf.Field{Number:2, Type:2, Value:protobuf.Bytes("!\xb8-\xc2\xeb\xb2MZ\xa9\xf8c\x1f\x04rfP")},
+protobuf.Field{Number:3, Type:2, Value:protobuf.Bytes("hulu")},
+protobuf.Field{Number:5, Type:2, Value:protobuf.Bytes("HD")},
+protobuf.Field{Number:5, Type:-2, Value:protobuf.Message{
+protobuf.Field{Number:9, Type:0, Value:protobuf.Varint(68)},
+}},
+}
---
+++ testdata/paramount.go
@@ -0,0 +1,6 @@
+protobuf.Message{
+protobuf.Field{Number:1, Type:0, Value:protobuf.Varint(1)},
+protobuf.Field{Number:2, Type:2, Value:protobuf.Bytes("=\xe0\xf3<\x1b\x8aOΖ\x1eکP\xe2\xe72")},
+protobuf.Field{Number:4, Type:2, Value:protobuf.Bytes("bqsJh_z7o4AR6ktui_9y8wIHqzEEqbhr")},
+protobuf.Field{Number:7, Type:0, Value:protobuf.Varint(1)},
+}
