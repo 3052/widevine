2aa0bd36c0928a37d61a4758863da2b70a423ba6
3052 <spen+3052@protonmail.com>
2024-04-11 18:29:43 -0500

    +func TestAmc(t *testing.T) {

--- cdm_test.go
+++ cdm_test.go
@@ -12,6 +12,90 @@ import (
    "testing"
 )
 
+func TestAmc(t *testing.T) {
+   key, err := request("amc", nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
+}
+
+func TestHulu(t *testing.T) {
+   key, err := request("hulu", nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
+}
+
+func TestMubi(t *testing.T) {
+   unwrap := func(b []byte) ([]byte, error) {
+      var s struct {
+         License []byte
+      }
+      err := json.Unmarshal(b, &s)
+      if err != nil {
+         return nil, err
+      }
+      return s.License, nil
+   }
+   key, err := request("mubi", unwrap)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
+}
+
+func TestNbc(t *testing.T) {
+   key, err := request("nbc", nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
+}
+
+func TestParamount(t *testing.T) {
+   key, err := request("paramount", nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
+}
+
+func TestPeacock(t *testing.T) {
+   key, err := request("peacock", nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
+}
+
+func TestRoku(t *testing.T) {
+   key, err := request("roku", nil)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
+}
+
+func TestStan(t *testing.T) {
+   unwrap := func(b []byte) ([]byte, error) {
+      var s struct {
+         License []byte
+      }
+      err := json.Unmarshal(b, &s)
+      if err != nil {
+         return nil, err
+      }
+      return s.License, nil
+   }
+   key, err := request("stan", unwrap)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%x\n", key)
+}
+
 func request(name string, unwrap unwrapper) ([]byte, error) {
    file, err := os.Open("testdata/" + name + ".bin")
    if err != nil {
@@ -68,88 +152,4 @@ func request(name string, unwrap unwrapper) ([]byte, error) {
    return key, nil
 }
 
-func TestHulu(t *testing.T) {
-   key, err := request("hulu", nil)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
-}
-
-func TestAmc(t *testing.T) {
-   key, err := request("amc", nil)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
-}
-
-func TestParamount(t *testing.T) {
-   key, err := request("paramount", nil)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
-}
-
-func TestRoku(t *testing.T) {
-   key, err := request("roku", nil)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
-}
-
-func TestNbc(t *testing.T) {
-   key, err := request("nbc", nil)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
-}
-
 type unwrapper func([]byte) ([]byte, error)
-
-func TestStan(t *testing.T) {
-   unwrap := func(b []byte) ([]byte, error) {
-      var s struct {
-         License []byte
-      }
-      err := json.Unmarshal(b, &s)
-      if err != nil {
-         return nil, err
-      }
-      return s.License, nil
-   }
-   key, err := request("stan", unwrap)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
-}
-
-func TestMubi(t *testing.T) {
-   unwrap := func(b []byte) ([]byte, error) {
-      var s struct {
-         License []byte
-      }
-      err := json.Unmarshal(b, &s)
-      if err != nil {
-         return nil, err
-      }
-      return s.License, nil
-   }
-   key, err := request("mubi", unwrap)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
-}
-
-func TestPeacock(t *testing.T) {
-   key, err := request("peacock", nil)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%x\n", key)
-}
