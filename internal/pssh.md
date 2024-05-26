9910e7517bc91cbacbe9b0b36edc17d299967227
3052 <spen+3052@protonmail.com>
2024-05-26 16:10:22 -0500

    new file:   internal/pssh/pssh.go

--- internal/pssh.md
+++ internal/pssh.md
@@ -1,3 +1,29 @@
 # pssh
 
 ## AMC+
+
+needs key ID. found in MPD and init
+
+## criterion
+
+needs key ID. found in MPD
+
+## ctv
+
+needs content ID
+
+## rakuten
+
+needs content ID. decoded:
+
+~~~
+cf34a03bbb4aa899df42c3cf7a6cb925-mc-0-137-0-0
+~~~
+
+is returned by `/v3/avod/streamings` and MPD. encoded:
+
+~~~
+NmMzRhMDNiYmI0YWE4OTlkZjQyYzNjZjdhNmNiOTI1LW1jLTAtMTM3LTAtMA
+~~~
+
+is returned by MPD.
---
+++ internal/pssh/pssh.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+   "encoding/base64"
+   "encoding/hex"
+   "flag"
+   "fmt"
+   "slices"
+)
+
+func prepend(src []byte, i int) string {
+   dst := append(make([]byte, i), src...)
+   return base64.RawStdEncoding.EncodeToString(dst)[i+1:]
+}
+
+func lower(src []byte) string {
+   return fmt.Sprintf("%x", src)
+}
+
+func double_escape(src []byte) string {
+   var dst []byte
+   for _, s := range src {
+      dst = fmt.Appendf(dst, `\\x%02x`, s)
+   }
+   return string(dst)
+}
+
+func upper(src []byte) string {
+   return fmt.Sprintf("%X", src)
+}
+
+func dash(src string) string {
+   dst := []byte(src)
+   dst = slices.Insert(dst, 8, '-')
+   dst = slices.Insert(dst, 13, '-')
+   dst = slices.Insert(dst, 18, '-')
+   dst = slices.Insert(dst, 23, '-')
+   return string(dst)
+}
+func main() {
+   base64_id := flag.String("b", "", "base64 ID")
+   hex_id := flag.String("h", "", "hex ID")
+   flag.Parse()
+   data, ok := func() ([]byte, bool) {
+      switch {
+      case *base64_id != "":
+         b, err := base64.StdEncoding.DecodeString(*base64_id)
+         if err != nil {
+            panic(err)
+         }
+         return b, true
+      case *hex_id != "":
+         b, err := hex.DecodeString(*hex_id)
+         if err != nil {
+            panic(err)
+         }
+         return b, true
+      }
+      return nil, false
+   }()
+   if ok {
+      fmt.Println(lower(data))
+      fmt.Println(upper(data))
+      fmt.Println(dash(lower(data)))
+      fmt.Println(dash(upper(data)))
+      fmt.Println(prepend(data, 0))
+      fmt.Println(prepend(data, 1))
+      fmt.Println(prepend(data, 2))
+      fmt.Println(double_escape(data))
+   } else {
+      flag.Usage()
+   }
+}
+
