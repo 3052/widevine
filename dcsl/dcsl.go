6a8c88718242ba43d6fb6f04816074ec16962d19
3052 <spen+3052@protonmail.com>
2024-12-25 19:38:16 -0600

    new file:   dcsl/new.go

--- dcsl/dcsl.go
+++ dcsl/dcsl.go
@@ -23,40 +23,6 @@ func (transport) RoundTrip(req *http.Request) (*http.Response, error) {
    return http.DefaultTransport.RoundTrip(req)
 }
 
-func (r resp_code) String() string {
-   return "x-dt-resp-code = " + codes[r]
-}
-
-type resp_code int
-
-var codes = map[resp_code]string{
-   0: "Success",
-   1000: "General Internal Error",
-   2000: "General Request Error",
-   3000: "General Request Authentication Error",
-   10001: "Bad Request",
-   3e4: "General DRM error",
-   4e4: "General Widevine Modular error",
-   40001: "Widevine Device Certificate Revocation (wv 127)",
-   40002: "Widevine Device Certificate Revocation - Permanently (wv 175)",
-   41e3: "General Widevine Classic error",
-   42e3: "General Playready error",
-   43e3: "General Fairplay error",
-   44e3: "General OMA error",
-   44001: "OMA Device registration failed",
-   45e3: "General CDRM error",
-   45001: "CDRM Device registration failed",
-   6e4: "CSL",
-   60001: "CSL - INVALID",
-   60100: "CSL - Denied by Stream Limiting",
-   7e4: "General Output Protection",
-   70001: "All keys filtered by EOP settings",
-   8e4: "TAKE DOWN",
-   80001: "TAKE DOWN - Denied by Take Down",
-   9e4: "General GBL error",
-   90001: "License delivery prohibited in your region",
-}
-
 type client_info struct {
    DrmVersion          *string
    HdcpSupport         string
@@ -91,93 +57,36 @@ func (c *client_info) String() string {
    return string(data)
 }
 
-///
+type resp_code int
 
-type drm_today func() (client_info, resp_code)
-
-func (d *drm_today) New(private_key, client_id []byte) error {
-   key_id, err := hex.DecodeString(raw_key_id)
-   if err != nil {
-      return err
-   }
-   var pssh widevine.PsshData
-   pssh.KeyIds = [][]byte{key_id}
-   var module widevine.Cdm
-   err = module.New(private_key, client_id, pssh.Marshal())
-   if err != nil {
-      return err
-   }
-   data, err := module.RequestBody()
-   if err != nil {
-      return err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://lic.staging.drmtoday.com/license-proxy-widevine/cenc",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return err
-   }
-   data, err = json.Marshal(map[string]string{
-      "merchant": "client_dev",
-      "userId":   "purchase",
-   })
-   if err != nil {
-      return err
-   }
-   req.Header.Set("dt-custom-data", base64.StdEncoding.EncodeToString(data))
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   data, err = base64.StdEncoding.DecodeString(
-      resp.Header.Get("x-dt-client-info"),
-   )
-   if err != nil {
-      return err
-   }
-   var info client_info
-   err = json.Unmarshal(data, &info)
-   if err != nil {
-      return err
-   }
-   code, err := strconv.Atoi(resp.Header.Get("x-dt-resp-code"))
-   if err != nil {
-      return err
-   }
-   *d = func() (client_info, resp_code) {
-      return info, resp_code(code)
-   }
-   return nil
+func (r resp_code) String() string {
+   return "x-dt-resp-code = " + codes[r]
 }
 
-func main() {
-   http.DefaultClient.Transport = transport{}
-   var f struct {
-      client_id string
-      private_key string
-   }
-   flag.StringVar(&f.client_id, "c", "", "client ID")
-   flag.StringVar(&f.private_key, "p", "", "private key")
-   flag.Parse()
-   if f.client_id != "" {
-      client_id, err := os.ReadFile(f.client_id)
-      if err != nil {
-         panic(err)
-      }
-      private_key, err := os.ReadFile(f.private_key)
-      if err != nil {
-         panic(err)
-      }
-      var today drm_today
-      err = today.New(private_key, client_id)
-      if err != nil {
-         panic(err)
-      }
-      info, code := today()
-      fmt.Print(&info, "\n", code, "\n")
-   } else {
-      flag.Usage()
-   }
+var codes = map[resp_code]string{
+   0: "Success",
+   1000: "General Internal Error",
+   2000: "General Request Error",
+   3000: "General Request Authentication Error",
+   10001: "Bad Request",
+   3e4: "General DRM error",
+   4e4: "General Widevine Modular error",
+   40001: "Widevine Device Certificate Revocation (wv 127)",
+   40002: "Widevine Device Certificate Revocation - Permanently (wv 175)",
+   41e3: "General Widevine Classic error",
+   42e3: "General Playready error",
+   43e3: "General Fairplay error",
+   44e3: "General OMA error",
+   44001: "OMA Device registration failed",
+   45e3: "General CDRM error",
+   45001: "CDRM Device registration failed",
+   6e4: "CSL",
+   60001: "CSL - INVALID",
+   60100: "CSL - Denied by Stream Limiting",
+   7e4: "General Output Protection",
+   70001: "All keys filtered by EOP settings",
+   8e4: "TAKE DOWN",
+   80001: "TAKE DOWN - Denied by Take Down",
+   9e4: "General GBL error",
+   90001: "License delivery prohibited in your region",
 }
