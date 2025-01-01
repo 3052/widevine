817f65768b7ec14fbce125a3772c7607d8e64614
3052 <spen+3052@protonmail.com>
2025-01-01 12:40:25 -0600

    deleted:    dcsl/main.go

--- dcsl/dcsl.go
+++ dcsl/dcsl.go
@@ -4,10 +4,51 @@ import (
    "41.neocities.org/widevine"
    "bytes"
    "encoding/json"
+   "flag"
+   "fmt"
    "net/http"
+   "os"
    "strconv"
 )
 
+func main() {
+   http.DefaultClient.Transport = transport{}
+   var f struct {
+      client_id string
+      private_key string
+   }
+   flag.StringVar(&f.client_id, "c", "", "client ID")
+   flag.StringVar(&f.private_key, "p", "", "private key")
+   flag.Parse()
+   if f.client_id != "" {
+      client_id, err := os.ReadFile(f.client_id)
+      if err != nil {
+         panic(err)
+      }
+      private_key, err := os.ReadFile(f.private_key)
+      if err != nil {
+         panic(err)
+      }
+      var license get_license
+      err = license.New(private_key, client_id)
+      if err != nil {
+         panic(err)
+      }
+      fmt.Println(&license)
+   } else {
+      flag.Usage()
+   }
+}
+
+// demo.unified-streaming.com/k8s/features
+const content_id = "fkj3ljaSdfalkr3j"
+
+type transport struct{}
+
+func (transport) RoundTrip(req *http.Request) (*http.Response, error) {
+   fmt.Println(req.URL)
+   return http.DefaultTransport.RoundTrip(req)
+}
 func (g *get_license) String() string {
    b := []byte("client max hdcp version = ")
    b = append(b, g.ClientMaxHdcpVersion...)
