a392a325e2abd2ed178f9338d26b2562a074050b
3052 <spen+3052@protonmail.com>
2025-01-04 09:35:25 -0600

    +type PsshData struct {

--- widevine_test.go
+++ widevine_test.go
@@ -3,7 +3,6 @@ package widevine
 import (
    "bytes"
    "encoding/base64"
-   "errors"
    "fmt"
    "io"
    "net/http"
@@ -11,45 +10,58 @@ import (
    "testing"
 )
 
-func decrypt(private_key, client_id []byte, wrap Wrapper) ([]byte, error) {
+func decrypt(private_key, client_id []byte, wrap Wrapper) error {
    key_id, err := base64.StdEncoding.DecodeString(test.key_id)
    if err != nil {
-      return nil, err
+      return err
    }
    var pssh PsshData
    pssh.KeyIds = [][]byte{key_id}
    var module Cdm
    err = module.New(private_key, client_id, pssh.Marshal())
    if err != nil {
-      return nil, err
+      return err
    }
    data, err := module.RequestBody()
    if err != nil {
-      return nil, err
+      return err
    }
    data, err = wrap.Wrap(data)
    if err != nil {
-      return nil, err
+      return err
    }
    var body ResponseBody
    err = body.Unmarshal(data)
    if err != nil {
-      return nil, err
+      return err
    }
    block, err := module.Block(body)
    if err != nil {
-      return nil, err
+      return err
    }
    containers := body.Container()
    for {
       container, ok := containers()
       if !ok {
-         return nil, errors.New("ResponseBody.Container")
+         return nil
       }
-      if bytes.Equal(container.Id(), key_id) {
-         return container.Key(block), nil
-      }
+      fmt.Printf(
+         "%v %q %x\n",
+         container.Type(), container.TrackLabel(), container.Key(block),
+      )
    }
+}
+
+type pluto struct{}
+
+var test = struct{
+   id     string
+   key_id string
+   url    string
+}{
+   id:     "675a0fa22678a50014690c3f",
+   key_id: "AAAAAGdaD6FuwTSRB/+yHg==",
+   url:    "pluto.tv/on-demand/movies/675a0fa22678a50014690c3f",
 }
 
 func (pluto) Wrap(data []byte) ([]byte, error) {
@@ -62,16 +74,6 @@ func (pluto) Wrap(data []byte) ([]byte, error) {
    }
    defer resp.Body.Close()
    return io.ReadAll(resp.Body)
-}
-
-var test = struct{
-   id     string
-   key_id string
-   url    string
-}{
-   id:     "675a0fa22678a50014690c3f",
-   key_id: "AAAAAGdaD6FuwTSRB/+yHg==",
-   url:    "pluto.tv/on-demand/movies/675a0fa22678a50014690c3f",
 }
 
 func TestPluto(t *testing.T) {
@@ -87,12 +89,8 @@ func TestPluto(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   key, err := decrypt(private_key, client_id, pluto{})
+   err = decrypt(private_key, client_id, pluto{})
    if err != nil {
       t.Fatal(err)
    }
-   fmt.Printf("%x\n", key)
 }
-
-type pluto struct{}
-
