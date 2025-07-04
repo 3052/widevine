90f383688d92dbd158613885d827cf12bf09f4d7
367 <367@tuta.io>
2025-07-03 21:59:29 -0500

    +const content_id = "fkj3ljaSdfalkr3j"

--- dcsl/dcsl.go
+++ dcsl/dcsl.go
@@ -11,51 +11,6 @@ import (
    "os"
 )
 
-func main() {
-   http.DefaultClient.Transport = transport{}
-   log.SetFlags(log.Ltime)
-   var client_id struct {
-      data []byte
-      name string
-   }
-   var private_key struct {
-      data []byte
-      name string
-   }
-   flag.StringVar(&client_id.name, "c", "", "client ID")
-   flag.StringVar(&private_key.name, "p", "", "private key")
-   flag.Parse()
-   if client_id.name != "" {
-      var err error
-      client_id.data, err = os.ReadFile(client_id.name)
-      if err != nil {
-         panic(err)
-      }
-      private_key.data, err = os.ReadFile(private_key.name)
-      if err != nil {
-         panic(err)
-      }
-      var license get_license
-      err = license.New(private_key.data, client_id.data)
-      if err != nil {
-         panic(err)
-      }
-      fmt.Println(&license)
-   } else {
-      flag.Usage()
-   }
-}
-
-type transport struct{}
-
-func (transport) RoundTrip(req *http.Request) (*http.Response, error) {
-   log.Println(req.Method, req.URL)
-   return http.DefaultTransport.RoundTrip(req)
-}
-
-// demo.unified-streaming.com/k8s/features
-const content_id = "fkj3ljaSdfalkr3j"
-
 func (g *get_license) New(private_key, client_id []byte) error {
    var pssh widevine.Pssh
    pssh.ContentId = []byte(content_id)
@@ -124,3 +79,47 @@ func (g *get_license) String() string {
    b = fmt.Append(b, "system id = ", g.SystemId)
    return string(b)
 }
+func main() {
+   http.DefaultClient.Transport = transport{}
+   log.SetFlags(log.Ltime)
+   var client_id struct {
+      data []byte
+      name string
+   }
+   var private_key struct {
+      data []byte
+      name string
+   }
+   flag.StringVar(&client_id.name, "c", "", "client ID")
+   flag.StringVar(&private_key.name, "p", "", "private key")
+   flag.Parse()
+   if client_id.name != "" {
+      var err error
+      client_id.data, err = os.ReadFile(client_id.name)
+      if err != nil {
+         panic(err)
+      }
+      private_key.data, err = os.ReadFile(private_key.name)
+      if err != nil {
+         panic(err)
+      }
+      var license get_license
+      err = license.New(private_key.data, client_id.data)
+      if err != nil {
+         panic(err)
+      }
+      fmt.Println(&license)
+   } else {
+      flag.Usage()
+   }
+}
+
+type transport struct{}
+
+func (transport) RoundTrip(req *http.Request) (*http.Response, error) {
+   log.Println(req.Method, req.URL)
+   return http.DefaultTransport.RoundTrip(req)
+}
+
+// demo.unified-streaming.com/k8s/features
+const content_id = "fkj3ljaSdfalkr3j"
